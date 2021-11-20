61550cde00ab622b5397f977680cf26871ea306a
JeremyVunCode4Fun <94739483+JeremyVun@users.noreply.github.com>
2021-11-20 16:45:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+"""
+Dictionary Challenge 1 - Jewels and Stones
+
+Description:
+You're given a string "jewels" representing the types of stones that are jewels, and a string "stones" representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Example:
+Input:
+  jewels = "aA"
+  stones = "aAAbbbb"
+Correct Output:
+  3
+"""
+
+
+########
+# Write your code in this function
+########
+def solution(jewels, stones):
+  pass
+
+
+########
+# This code wil run your solution against a bunch of test cases
+########
+from tests.runner import run
+
+if __name__ == "__main__":
+  run(solution)
